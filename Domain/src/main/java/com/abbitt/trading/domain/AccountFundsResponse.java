c07548545fefaf58e62a68d8651391440daf2543
DemonDriverDan <danielabbitt@gmail.com>
2016-03-06 19:45:32 +0000

    Add AccountFundsResponse class

---
+++ Domain/src/main/java/com/abbitt/trading/domain/AccountFundsResponse.java
@@ -0,0 +1,46 @@
+package com.abbitt.trading.domain;
+
+
+public class AccountFundsResponse {
+
+    private final double availableToBetBalance;
+    private final double exposure;
+    private final double retainedCommission;
+    private final double exposureLimit;
+    private final double discountRate;
+    private final int pointsBalance;
+
+    public AccountFundsResponse(double availableToBetBalance, double exposure, double retainedCommission,
+                                double exposureLimit, double discountRate, int pointsBalance) {
+        this.availableToBetBalance = availableToBetBalance;
+        this.exposure = exposure;
+        this.retainedCommission = retainedCommission;
+        this.exposureLimit = exposureLimit;
+        this.discountRate = discountRate;
+        this.pointsBalance = pointsBalance;
+    }
+
+    public double getAvailableToBetBalance() {
+        return availableToBetBalance;
+    }
+
+    public double getExposure() {
+        return exposure;
+    }
+
+    public double getRetainedCommission() {
+        return retainedCommission;
+    }
+
+    public double getExposureLimit() {
+        return exposureLimit;
+    }
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
+
+    public int getPointsBalance() {
+        return pointsBalance;
+    }
+}
